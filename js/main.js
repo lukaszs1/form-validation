d3ac3c14ccbf4ca096b57f752f6caa57a7cdf705
Łukasz Stolarski <lukasz.stolarski@intive.com>
2017-01-22 23:45:13 +0100

    form falidation without jquery validate

--- js/main.js
+++ js/main.js
@@ -1,58 +1,92 @@
 $(document).ready(function () {
-    var formValid = false;
-    
+
     $('input').on('blur', function () {
         var fieldType = $(this).data('fieldtype');
-        var fieldValue = $(this).val();
-        if (!validate(fieldType, fieldValue)) {
-            $(this).next('span').show();
-            $(this).attr('data-has-error', true);
+        var val = $(this).val();
+
+        switch (fieldType) {
+            case 'creditCard':
+                if (!validators.creditCard(val)) {
+                    formValid.cc = false;
+                    $(this).next().show();
+                } else {
+                    formValid.cc = true;
+                    $(this).next().hide();
+                }
+                break;
+            case 'dateString':
+                if (!validators.dateString(val)) {
+                    formValid.date = false;
+                    $(this).next().show();
+                } else {
+                    formValid.date = true;
+                    $(this).next().hide();
+                }
+                break;
+            case 'email':
+                if (!validators.email(val)) {
+                    formValid.email = false;
+                    $(this).next().show();
+                } else {
+                    formValid.email = true;
+                    $(this).next().hide();
+                }
+                break;
+            case 'cvv':
+                if (!validators.cvv(val)) {
+                    formValid.cvv = false;
+                    $(this).next().show();
+                } else {
+                    formValid.cvv = true;
+                    $(this).next().hide();
+                }
+                break;
+
         }
-        else {
-            $(this).attr('data-has-error', false);
-        }
+
     });
 
     $('input').on('focus', function () {
-        $(this).next('span').hide();
-    })
+        $('#submit').removeAttr('disabled', 'disabled');
+    });
 
     $('form').on('submit', function (e) {
         e.preventDefault();
-        if ($('input').attr('data-has-error') === true) {
-            formValid = true;
-        }
-        else {
-            formValid = false;
-        }
-        if (!formValid) {
+
+        if (!formValid.cc || !formValid.date || !formValid.email || !formValid.cvv) {
+            $('#submit').attr('disabled', 'disabled');
             return false;
         }
-        $('this').html('<h2>Loading...</h2>');
-        setTimeout(function () {
+        $('form').html('<h2>Loading...</h2>');
+        var x = setTimeout(function () {
             $('form').html('<h1>Form submited</h1>');
-        },2000)
+        }, 3000)
     })
 
 });
 
+var formValid = {
+    cc: false,
+    date: false,
+    email: true,
+    cvv: false
+};
 
-function validate(fieldType, fieldValue) {
-    if (fieldType) {
-        switch (fieldType) {
-            case 'creditCard':
-                return is.creditCard(fieldValue);
-            case 'dateString':
-                return is.dateString(fieldValue);
-            case 'email':
-                return is.email(fieldValue);
-            case 'cvv':
-                is.within(fieldValue, 100, 999);
-            default:
-                return false;
-        }
+var validators = {
+
+    email: function (val) {
+        return is.email(val);
+    },
+    creditCard: function (val) {
+        return is.creditCard(val);
+    },
+    dateString: function (val) {
+        return is.dateString(val);
+    },
+    cvv: function (val) {
+        console.log(val);
+        return is.within(parseInt(val), 100, 999);
+
     }
-
-
-}
+};
 
